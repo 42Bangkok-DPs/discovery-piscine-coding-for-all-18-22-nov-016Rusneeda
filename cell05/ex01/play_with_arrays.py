e13d7084390b8c1be424f6696deceed2b13a6e76
rusneedakuwi <123723045+016Rusneeda@users.noreply.github.com>
2024-11-21 03:55:41 +0000

    commit

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+  
+new_array = []
+for num in original_array:
+    new_array.append(num + 2)
+
+  
+print("Original array:", original_array)
+print("New array:", new_array)
+
+# 1.โจทย์คือให้สร้างตัวแปรเพื่อเก็บอาเรย์ 2.สร้างตัวแปรอีกตัวเป็นเก็บอาเรย์ตัวใหม่ (ต้องเอาค่าอาเรย์ตัวเก่ามา+2) 

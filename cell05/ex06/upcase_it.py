1f9e7099489d76ac4e0da55ae86c6212ca07393d
rusneedakuwi <123723045+016Rusneeda@users.noreply.github.com>
2024-11-22 10:39:48 +0000

    commit

--- cell05/ex06/upcase_it.py
+++ cell05/ex06/upcase_it.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 import sys
 
-# ตรวจสอบว่ามีพารามิเตอร์ 1 ตัวหรือไม่
 if len(sys.argv) == 2:
-    # ถ้ามีพารามิเตอร์ 1 ตัว ให้แปลงเป็นตัวอักษรใหญ่และแสดงผล
     print(sys.argv[1].upper())
 else:
-    # ถ้าจำนวนพารามิเตอร์ไม่เท่ากับ 1 ให้แสดง 'none'
     print("none")
-    
+
+   
 """  ใช้คำสั่ง
 ./upcase_it.py | cat -e
 none$
